b68e9887529bec9154f82bee2ad4806724125143
gizem <132500612+babyshark023@users.noreply.github.com>
2024-09-18 15:23:05 +0300

    update footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,13 +1,11 @@
+"use client";
 import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-  <div>
-
-      </div>
-    
+    <footer >
+    </footer>
   );
 };
-
 
 export default Footer;
